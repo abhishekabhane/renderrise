be76b57ab2f80742299de84c11c003c2dc18a28d
abhaneabhishek <abhaneabhishek@gmail.com>
2025-04-18 22:04:30 +0530

    Added contact us functionality and exterior interior images page and content changes

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+require('dotenv').config();
+
 const app = express();
-const path = require('path');
+// const path = require('path');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res) {
     res.render("index");
@@ -22,8 +26,39 @@ app.get("/contactus", function(req, res) {
     res.render("contactus");
 });
 
-app.get("/Works", function(req, res) {
-    res.render("Works");
+app.get("/Exterior", function(req, res) {
+    res.render("Exterior");
+});
+
+app.get("/Interior", function(req, res) {
+    res.render("Interior");
+});
+
+app.post('/send-email', (req, res) => {
+    const { floating_first_name, floating_email, floating_message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    const mailOptions = {
+        from: floating_email,
+        to: process.env.EMAIL_USER,
+        subject: `New Inquiry from ${floating_first_name}`,
+        text: `Name: ${floating_first_name}\nEmail: ${floating_email}\n\nMessage:\n${floating_message}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            return res.status(500).send('Something went wrong.');
+        }
+        res.send('Email sent successfully!');
+    });
 });
 
 let port = process.env.PORT;
